434846b665fe6ca44236b9cc195ffb647ae9946c
IvanAntasyuk <Pikin64@gmail.com>
2021-08-27 23:03:18 +0300

    Task one add background

--- core/src/com/mygdx/game/MyFirstGame.java
+++ core/src/com/mygdx/game/MyFirstGame.java
@@ -6,26 +6,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyFirstGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private SpriteBatch batch;
+    private Texture img;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("135854.jpg");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.35f, 0.66f, 0.13f, 1);
+        batch.begin();
+        batch.draw(img, 0, 0,700,512);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
