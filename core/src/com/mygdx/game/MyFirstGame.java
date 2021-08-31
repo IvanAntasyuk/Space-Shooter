dcc242768ce0f3d77c736f96ccfe20789b00ca7e
IvanAntasyuk <Pikin64@gmail.com>
2021-08-31 13:53:52 +0300

    Task two

--- core/src/com/mygdx/game/MyFirstGame.java
+++ core/src/com/mygdx/game/MyFirstGame.java
@@ -1,31 +1,15 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.screen.MenuScreen;
 
-public class MyFirstGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture img;
-
+public class MyFirstGame extends Game {
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("135854.jpg");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.35f, 0.66f, 0.13f, 1);
-        batch.begin();
-        batch.draw(img, 0, 0,700,512);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
+        setScreen(new MenuScreen());
     }
 }
