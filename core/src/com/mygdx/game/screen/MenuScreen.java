61172c87ad23e65ed7eaccac6d1a868fc157d71b
IvanAntasyuk <Pikin64@gmail.com>
2021-09-05 23:45:44 +0300

    lesson 3

--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -5,52 +5,58 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.BaseScreen;
+import com.mygdx.game.math.Rect;
+import com.mygdx.game.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
-    private static final float V_LEN = 1.5f;
     private Texture img;
-    private Vector2 touch;
-    private Vector2 pos;
-    private Vector2 v;
-    private Vector2 temp;
+    private Texture bg;
 
+    private Background background;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("135854.jpg");
-        touch = new Vector2();
-        pos = new Vector2();
-        v = new Vector2();
-        temp = new Vector2();
+        img = new Texture("badlogic.jpg");
+        bg = new Texture("textures/bg.png");
+
+        background = new Background(bg);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        temp.set(touch);
-        if (temp.sub(pos).len()>v.len()){
-            pos.add(v);
-        }else {
-            pos.set(touch);
-        }
-        batch.begin();
-        batch.draw(img, pos.x, pos.y, 300, 300);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
     public void dispose() {
         super.dispose();
         img.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
-        return super.touchDown(screenX, screenY, pointer, button);
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
+    }
+
+    private void update(float delta) {
 
     }
 
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        batch.draw(img, 0, 0, 0.3f, 0.3f);
+        batch.end();
+    }
 }
+
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,5 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyFirstGame(), config);
+		config.width = 500;
+		config.height = 600;
+		config.resizable = false;
 	}
 }
